6fe0d71bb748f3bff45583ea1cfdbf5e4eb64b59
uonr <34709816+uonr@users.noreply.github.com>
2023-09-02 10:31:47 +0900

    feat: git

--- nixos.nix
+++ nixos.nix
@@ -4,7 +4,7 @@
     i18n.defaultLocale = "en_US.UTF-8";
     programs.nix-ld.enable = true;
     programs.mosh.enable = true;
-    environment.systemPackages = with pkgs; [ wget nil nixfmt htop tmux ];
+    environment.systemPackages = with pkgs; [ wget git nil nixfmt htop tmux ];
     documentation.enable = false;
   };
 }
